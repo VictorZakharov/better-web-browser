303877c77f979db4938aaf66faec086dfa2fba79
Victor Zakharov <neolisk@gmail.com>
2026-08-24 11:48:23 -0400

    Shorten public alpha CI critical path

--- benchmarks/compare.ps1
+++ benchmarks/compare.ps1
@@ -3,6 +3,8 @@ param(
     [ValidateRange(1, 10)] [int] $Iterations = 3,
     [string[]] $Fixture = @(),
     [string] $OutputDirectory,
+    [ValidateSet('debug', 'release', 'performance')]
+    [string] $BuildProfile = 'release',
     [switch] $Live,
     [switch] $SkipBuild
 )
